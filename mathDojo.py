321863bdcf23c26e2f9758b869e06961b6dfbc49
Jimmy <chen.jimmym@gmail.com>
2017-10-05 13:41:25 -0700

    done

--- mathDojo.py
+++ mathDojo.py
@@ -3,15 +3,23 @@ class mathDojo(object):
         self.num = 0
     def add(self, *arg):
         for data in arg:
-            self.num += data
+            if isinstance(data, list) or isinstance(data, tuple):
+                for datas in data:
+                    self.num += datas
+            else:
+                self.num += data
         return self
     def subtract(self, *arg):
         for data in arg:
-            self.num -= data
+            if isinstance(data, list) or isinstance(data, tuple):
+                for datas in data:
+                    self.num -= datas
+            else:
+                self.num -= data
         return self
     def result(self):
         print self.num
         return self
 
 md = mathDojo()
-md.add(2).add(2,5).subtract(3,2).result()
+md.add([1], 3,4).add([3,5,7,8], (2,4.3,1.25)).subtract(2, (2,3), [1.1,2.3]).result()
